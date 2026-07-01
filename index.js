384a858327cecd939601c63c64a8bfd6426b10b7
Chuos24 <129564525+Chuos24@users.noreply.github.com>
2026-07-01 10:50:23 +0200

    cleanup: remove index.js (Node.js legacy)

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const app = require('./src/app');
-const config = require('./src/config');
-
-app.listen(config.port, () => {
-  console.log(`[ConflictZero Backend] v2.0.0 corriendo en puerto ${config.port}`);
-  console.log(`[ConflictZero Backend] Entorno: ${config.nodeEnv}`);
-});
